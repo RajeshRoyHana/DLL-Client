b12a23f9df3b147097362a97d69e5bee92e2f15f
Shuhag Mia <shuhag.mia@hana-ltd.com>
2026-05-21 16:10:23 +0600

    update the code syntex

--- src/app/pages/home/home-office-location-component/home-office-location-component.ts
+++ src/app/pages/home/home-office-location-component/home-office-location-component.ts
@@ -7,45 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './home-office-location-component.css',
 })
 export class HomeOfficeLocationComponent {
-  
-offices = [
-    {
-      country: 'Bangladesh',
-      address: 'Level 8, Dhanmondi, Dhaka 1209, Bangladesh',
-      email: 'contact@dllsoft.com',
-      phone: '+880 1700-000000'
-    },
-    {
-      country: 'USA',
-      address: '7426 Business Ave, New York, NY 10001',
-      email: 'usa@dllsoft.com',
-      phone: '+1 212-555-7890'
-    },
-    {
-      country: 'Germany',
-      address: 'Berlin Tech Park, Berlin, Germany',
-      email: 'eu@dllsoft.com',
-      phone: '+49 30 123456'
-    },
-    {
-      country: 'UAE',
-      address: 'Dubai Internet City, Dubai, UAE',
-      email: 'uae@dllsoft.com',
-      phone: '+971 50 1234567'
-    },
-    {
-      country: 'Malaysia',
-      address: 'Kuala Lumpur Tech Hub, Malaysia',
-      email: 'asia@dllsoft.com',
-      phone: '+60 111 234567'
-    },
-    {
-      country: 'Japan',
-      address: 'Shibuya Tech Center, Tokyo, Japan',
-      email: 'jp@dllsoft.com',
-      phone: '+81 90 1234 5678'
-    }
-  ];
 
 
 }
