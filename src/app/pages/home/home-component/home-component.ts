2fa2c34c7d1893f9b875a28f4e1b9f92548b6b82
Shuhag Mia <shuhag.mia@hana-ltd.com>
2026-05-21 12:12:10 +0600

    update home page

--- src/app/pages/home/home-component/home-component.ts
+++ src/app/pages/home/home-component/home-component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { HomeHeroComponent } from "../home-hero-component/home-hero-component";
 import { HomeIntroComponent } from '../home-intro-component/home-intro-component';
 import { HomeCarouselComponent } from '../home-carousel-component/home-carousel-component';
+import { HomeServiceComponent } from '../home-service-component/home-service-component';
+import { HomeStrategyComponent } from '../home-strategy-component/home-strategy-component';
 
 @Component({
   selector: 'app-home-component',
-  imports: [HomeHeroComponent,HomeIntroComponent, HomeCarouselComponent],
+  imports: [HomeHeroComponent,HomeIntroComponent, HomeCarouselComponent,HomeServiceComponent,HomeStrategyComponent],
   templateUrl: './home-component.html',
   styleUrl: './home-component.css',
 })
---
+++ src/app/pages/home/home-service-component/home-service-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-service-component',
+  imports: [],
+  templateUrl: './home-service-component.html',
+  styleUrl: './home-service-component.css',
+})
+export class HomeServiceComponent {
+
+}
