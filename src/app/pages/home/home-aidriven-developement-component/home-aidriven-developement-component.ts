9cccb9369a0b45a3eef6566415a5e02c626963a7
Shuhag Mia <shuhag.mia@hana-ltd.com>
2026-05-21 14:21:44 +0600

    updated: office-location,our products, ai-driven updated on homve page

---
+++ src/app/pages/home/home-aidriven-developement-component/home-aidriven-developement-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-aidriven-developement-component',
+  imports: [],
+  templateUrl: './home-aidriven-developement-component.html',
+  styleUrl: './home-aidriven-developement-component.css',
+})
+export class HomeAidrivenDevelopementComponent {
+
+}
--- src/app/pages/home/home-component/home-component.ts
+++ src/app/pages/home/home-component/home-component.ts
@@ -4,10 +4,13 @@ import { HomeIntroComponent } from '../home-intro-component/home-intro-component
 import { HomeCarouselComponent } from '../home-carousel-component/home-carousel-component';
 import { HomeServiceComponent } from '../home-service-component/home-service-component';
 import { HomeStrategyComponent } from '../home-strategy-component/home-strategy-component';
+import { HomeOfficeLocationComponent } from '../home-office-location-component/home-office-location-component';
+import { HomeProductsComponent } from '../home-products-component/home-products-component';
+import { HomeAidrivenDevelopementComponent } from '../home-aidriven-developement-component/home-aidriven-developement-component';
 
 @Component({
   selector: 'app-home-component',
-  imports: [HomeHeroComponent,HomeIntroComponent, HomeCarouselComponent,HomeServiceComponent,HomeStrategyComponent],
+  imports: [HomeHeroComponent,HomeIntroComponent, HomeCarouselComponent,HomeServiceComponent,HomeStrategyComponent,HomeOfficeLocationComponent,HomeProductsComponent,HomeAidrivenDevelopementComponent],
   templateUrl: './home-component.html',
   styleUrl: './home-component.css',
 })
