a324d2b288655f08df47acbfc2ca19b032861a9d
Rajesh Kumar Roy <rajesh.roy@hana-ltd.com>
2026-06-01 16:57:53 +0600

    navbar mobile view fix

--- src/app/Shared/components/navbar-component/navbar-component.ts
+++ src/app/Shared/components/navbar-component/navbar-component.ts
@@ -1,13 +1,31 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, NavigationEnd, RouterLink } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-navbar-component',
-  imports: [RouterLink,NgOptimizedImage],
+  imports: [RouterLink, NgOptimizedImage],
   templateUrl: './navbar-component.html',
   styleUrl: './navbar-component.css',
 })
 export class NavbarComponent {
+  isContactPage = false;
 
+  constructor(private router: Router) {
+    this.isContactPage = this.router.url === '/contact';
+    this.router.events.pipe(filter((event): event is NavigationEnd => event instanceof NavigationEnd))
+      .subscribe(event => {
+        this.isContactPage = event.urlAfterRedirects === '/contact';
+      });
+  }
+
+  handleServicesClick(event: MouseEvent) {
+    if (window.innerWidth <= 991.98) {
+      event.preventDefault();
+      event.stopImmediatePropagation();
+      const toggleButton = document.getElementById('servicesDropdownToggle');
+      toggleButton?.click();
+    }
+  }
 }
