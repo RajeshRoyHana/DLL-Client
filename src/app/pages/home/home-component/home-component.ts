1c7c32fc491fdf97ea4ff264645492ef8149aeac
Rajesh Kumar Roy <rajesh.roy@hana-ltd.com>
2026-06-18 11:52:01 +0600

    Remove carousel from home

--- src/app/pages/home/home-component/home-component.ts
+++ src/app/pages/home/home-component/home-component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { HomeHeroComponent } from "../home-hero-component/home-hero-component";
 import { HomeIntroComponent } from '../home-intro-component/home-intro-component';
-import { HomeCarouselComponent } from '../home-carousel-component/home-carousel-component';
 import { HomeServiceComponent } from '../home-service-component/home-service-component';
 import { HomeStrategyComponent } from '../home-strategy-component/home-strategy-component';
 import { HomeOfficeLocationComponent } from '../home-office-location-component/home-office-location-component';
@@ -10,7 +9,7 @@ import { HomeAidrivenDevelopementComponent } from '../home-aidriven-developement
 
 @Component({
   selector: 'app-home-component',
-  imports: [HomeHeroComponent,HomeIntroComponent, HomeCarouselComponent,HomeServiceComponent,HomeStrategyComponent,HomeOfficeLocationComponent,HomeProductsComponent,HomeAidrivenDevelopementComponent],
+  imports: [HomeHeroComponent,HomeIntroComponent,HomeServiceComponent,HomeStrategyComponent,HomeOfficeLocationComponent,HomeProductsComponent,HomeAidrivenDevelopementComponent],
   templateUrl: './home-component.html',
   styleUrl: './home-component.css',
 })
