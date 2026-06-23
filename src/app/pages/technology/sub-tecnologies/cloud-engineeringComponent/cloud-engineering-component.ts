5d500496ddd5acb48c7a65899b50363bdf5743d5
Rajesh Kumar Roy <rajesh.roy@hana-ltd.com>
2026-06-23 17:54:27 +0600

    feat: Add Security Engineering and Technology Components
    - Implemented Security Engineering Component with HTML and TypeScript files.
    - Created a comprehensive layout for the Security Engineering page, including sections for the hero, pillars, delivery cycle, and contact.
    - Developed Technology Component with a structured layout for showcasing technical services and related content.
    - Added CSS styles for both components to ensure responsive design and visual consistency.

---
+++ src/app/pages/technology/sub-tecnologies/cloud-engineeringComponent/cloud-engineering-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cloud-engineering-details',
+  imports: [],
+  templateUrl: './cloud-engineering-component.html',
+  styleUrl: './cloud-engineering-component.css',
+})
+export class CloudEngineeringComponent {
+
+}
---
+++ src/app/pages/technology/sub-tecnologies/security-engineering-component/security-engineering-component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-security-engineering-component',
+  imports: [CommonModule],
+  templateUrl: './security-engineering-component.html',
+  styleUrl: './security-engineering-component.css',
+})
+export class SecurityEngineeringComponent {
+
+}
